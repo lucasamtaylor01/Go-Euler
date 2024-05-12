cd780f93290d4028f33c96a40302963250ff81e3
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-12 13:04:36 -0300

    Update 08_Exercise
    I really hate deal with .txt files

--- 00_Euler_Project/Exercise_08/08_Exercise.go
+++ 00_Euler_Project/Exercise_08/08_Exercise.go
@@ -1,15 +1,28 @@
-/* By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
-
-What is the 10001st prime number? */
-
 package main
 
 import (
+	"fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	file, ferr := os.Open("number.txt")
-	//fmt.Println("Solution: ", candidate-1)
+	b, err := os.ReadFile("number.txt") // just pass the file name
+	if err != nil {
+		fmt.Print(err)
+	}
+	var number_list []int
+	for i := 0; i < len(b); i++ {
+		n, err := strconv.Atoi(string(b[i]))
+		if err != nil {
+			fmt.Print("ué")
+		} else {
+			number_list = append(number_list, n)
+		}
 
+	}
+
+	for i := 0; i < len(number_list); i++ {
+		fmt.Print(number_list[i])
+	}
 }
