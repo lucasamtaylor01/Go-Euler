64b546b977bcda68e6be94731a6db9b0634b8a87
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-12 13:06:20 -0300

    Update 08_Exercise.go

--- 00_Euler_Project/Exercise_08/08_Exercise.go
+++ 00_Euler_Project/Exercise_08/08_Exercise.go
@@ -7,21 +7,27 @@ import (
 )
 
 func main() {
+
+	//Read file
 	b, err := os.ReadFile("number.txt") // just pass the file name
 	if err != nil {
 		fmt.Print(err)
 	}
+	// Create a var slice
 	var number_list []int
+
+	// Convertion: bytes -> str -> int
 	for i := 0; i < len(b); i++ {
 		n, err := strconv.Atoi(string(b[i]))
 		if err != nil {
-			fmt.Print("ué")
+			fmt.Print(err)
 		} else {
 			number_list = append(number_list, n)
 		}
 
 	}
 
+	// Show the int values
 	for i := 0; i < len(number_list); i++ {
 		fmt.Print(number_list[i])
 	}
