0cdad0a01e2572f681f64c6a8ad774d3989aaecd
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-13 13:03:18 -0300

    Exercise 09 Attempt

---
+++ 00_Euler_Project/Exercise_09/09_Exercise.go
@@ -0,0 +1,24 @@
+/* Fist, I tried with pen and paper, but after some calculations appeared some complex numbers
+
+a = m² - n²
+b = 2mn
+c = m² + n²
+
+a+b+c = 1000
+m² - n² + 2mn + m² + n² = 1000
+2m² + 2mn = 1000
+m² + mn² - 500 = 0
+
+\Delta = 0
+n² - 4.1.(-500) = 0
+n² = \sqrt(-2000)
+
+...
+
+*/
+
+package main
+
+func main() {
+
+}
