2425c81f9bc3bb047d0d877d8bfefb0a317bd2ac
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-14 01:13:34 +0000

    Exercise 09 upload
    That was my favorite!

--- 00_Euler_Project/Exercise_09/09_Exercise.go
+++ 00_Euler_Project/Exercise_09/09_Exercise.go
@@ -1,24 +1,20 @@
-/* Fist, I tried with pen and paper, but after some calculations appeared some complex numbers
-
-a = m² - n²
-b = 2mn
-c = m² + n²
-
-a+b+c = 1000
-m² - n² + 2mn + m² + n² = 1000
-2m² + 2mn = 1000
-m² + mn² - 500 = 0
-
-\Delta = 0
-n² - 4.1.(-500) = 0
-n² = \sqrt(-2000)
-
-...
-
-*/
+/* A Pythagorean triplet is a set of three natural numbers, a < b < c, for which, a^2 + b^2 = c^2.
+For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+There exists exactly one Pythagorean triplet for which a + b + c = 1000.Find the product abc.*/
 
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	var ind int = 0
+	for m := 21; ind == 0; m-- {
+		for n := 0; n < m; n++ {
+			if m*(m+n) == 500 {
+				var a, b, c int = (m * m) - (n * n), 2 * m * n, (m * m) + (n * n)
+				fmt.Println("Solution: ", a*b*c)
+				ind = 1
+			}
+		}
+	}
 }
