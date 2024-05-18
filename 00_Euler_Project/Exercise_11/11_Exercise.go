033d97517187fa698c2c0d4667ed1c2fa22f47f5
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-17 23:55:16 -0300

    Organizing

--- 00_Euler_Project/Exercise_11/11_Exercise.go
+++ 00_Euler_Project/Exercise_11/11_Exercise.go
@@ -6,7 +6,7 @@ import (
 	"strconv"
 )
 
-func horizontal_verify(index int, length int) bool {
+func HorizontalVerify(index int, length int) bool {
 	if index+3 > length-1 {
 		return false
 	}
@@ -18,30 +18,38 @@ func horizontal_verify(index int, length int) bool {
 	return true
 }
 
-func vertical_verify(index int, length int) bool {
-	if index+60 > length-1 {
+func VerticalVerify(index int, length int) bool {
+	if index+61 > length {
 		return false
 	}
 	return true
 }
 
-func down_diagonal_verify(index int, length int) bool {
-	if index+60+3 > length-1 {
+func DiagonalTypeOneVerify(index int, length int) bool {
+	if index+64 > length {
 		return false
 	}
 	return true
 }
 
-func up_diagonal_verify(index int) bool {
-	if index-60-3 <= 0 {
+func DiagonalTypeTwoVerify(index int) bool {
+	if index-63 <= 0 {
 		return false
 	}
 	return true
+}
+
+func UpdateGreatestList(great_list [4]int, candidate [4]int) [4]int {
+	for i := 0; i < 4; i++ {
+		great_list[i] = candidate[i]
+	}
+
+	return great_list
 }
 
 func main() {
 
-	//Read file
+	// Read file
 	b, err := os.ReadFile("20grid.txt")
 	if err != nil {
 		fmt.Print(err)
@@ -51,7 +59,7 @@ func main() {
 	var number_list []int
 
 	// Convertion: bytes -> str -> int
-	for i := 0; i < len(b)-1; i++ { // len b?
+	for i := 0; i < len(b)-1; i++ {
 		if b[i] >= 48 && b[i] <= 57 && b[i+1] >= 48 && b[i+1] <= 57 {
 			c := string(b[i]) + string(b[i+1])
 			n, err := strconv.Atoi(c)
@@ -62,78 +70,76 @@ func main() {
 			}
 		}
 	}
-	var greatest, length int = 0, len(number_list)
+
+	var greatest int = 0
 	var great_list [4]int
 
-	for i := 0; i < length; i++ {
-		if horizontal_verify(i, length) {
+	for i := 0; i < len(number_list); i++ {
+		if HorizontalVerify(i, len(number_list)) {
+
 			var product int = 1
 			var candidate [4]int
+
 			for counter := 0; counter < 4; counter++ {
 				product *= number_list[i+counter]
 				candidate[counter] = number_list[i+counter]
 			}
 
-			if product > greatest {
+			if greatest < product {
 				greatest = product
-				for i := 0; i < 4; i++ {
-					great_list[i] = candidate[i]
-				}
+				great_list = UpdateGreatestList(great_list, candidate)
 			}
 		}
 
-		if vertical_verify(i, length) {
+		if VerticalVerify(i, len(number_list)) {
 			var product int = 1
 			var candidate [4]int
+
 			for counter := 0; counter < 4; counter++ {
 				product *= number_list[i+20*counter]
 				candidate[counter] = number_list[i+20*counter]
 			}
 
-			if product > greatest {
+			if greatest < product {
 				greatest = product
-				for i := 0; i < 4; i++ {
-					great_list[i] = candidate[i]
-				}
+				great_list = UpdateGreatestList(great_list, candidate)
 			}
 		}
 
-		if down_diagonal_verify(i, length) {
+		if DiagonalTypeOneVerify(i, len(number_list)) {
+
 			var product int = 1
 			var candidate [4]int
+
 			for counter := 0; counter < 4; counter++ {
 				product *= number_list[i+20*counter-counter]
 				candidate[counter] = number_list[i+20*counter+counter]
 			}
-			fmt.Println(candidate)
-			if product > greatest {
+
+			if greatest < product {
 				greatest = product
-				for i := 0; i < 4; i++ {
-					great_list[i] = candidate[i]
-				}
+				great_list = UpdateGreatestList(great_list, candidate)
 			}
 		}
 
-		if up_diagonal_verify(i) {
+		if DiagonalTypeTwoVerify(i) {
+
 			var product int = 1
 			var candidate [4]int
+
 			for counter := 0; counter < 4; counter++ {
 				product *= number_list[i-20*counter-counter]
 				candidate[counter] = number_list[i-20*counter-counter]
 			}
-			 // fmt.Println(candidate)
-			if product > greatest {
+
+			if greatest < product {
 				greatest = product
-				for i := 0; i < 4; i++ {
-					great_list[i] = candidate[i]
-				}
+				great_list = UpdateGreatestList(great_list, candidate)
 			}
 		}
 	}
+
 	fmt.Println(greatest)
 	fmt.Println(great_list)
 
 }
-/* 70600674
-[89 69 7 46]
-/*
