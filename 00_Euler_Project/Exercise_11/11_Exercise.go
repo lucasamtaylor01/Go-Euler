efc8dd6a50090b80cb7ddbfb854ca43c5af324b2
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-18 00:15:27 -0300

    Add coments

--- 00_Euler_Project/Exercise_11/11_Exercise.go
+++ 00_Euler_Project/Exercise_11/11_Exercise.go
@@ -6,6 +6,7 @@ import (
 	"strconv"
 )
 
+// Checking if the current element has three horizontally adjacent elements
 func HorizontalVerify(index int, length int) bool {
 	if index+3 > length-1 {
 		return false
@@ -18,6 +19,7 @@ func HorizontalVerify(index int, length int) bool {
 	return true
 }
 
+// Checking if the current element has three vertically adjacent elements
 func VerticalVerify(index int, length int) bool {
 	if index+61 > length {
 		return false
@@ -25,6 +27,7 @@ func VerticalVerify(index int, length int) bool {
 	return true
 }
 
+// Checking if the current element has three diagonal (NW-SE) adjacent elements
 func DiagonalTypeOneVerify(index int, length int) bool {
 	if index+64 > length {
 		return false
@@ -32,6 +35,7 @@ func DiagonalTypeOneVerify(index int, length int) bool {
 	return true
 }
 
+// Checking if the current element has three diagonal (SW-NE) adjacent elements
 func DiagonalTypeTwoVerify(index int) bool {
 	if index-63 <= 0 {
 		return false
@@ -39,6 +43,7 @@ func DiagonalTypeTwoVerify(index int) bool {
 	return true
 }
 
+// Update the great_list if there's a bigger product
 func UpdateGreatestList(great_list [4]int, candidate [4]int) [4]int {
 	for i := 0; i < 4; i++ {
 		great_list[i] = candidate[i]
@@ -71,20 +76,25 @@ func main() {
 		}
 	}
 
+	// Creation of variables to store the greatest product and the four elements that originate it
 	var greatest int = 0
 	var great_list [4]int
 
+	// Loop to search on the grid the greatest product
 	for i := 0; i < len(number_list); i++ {
 		if HorizontalVerify(i, len(number_list)) {
 
+			// Creation of variables to analyse the candidates to greatest product
 			var product int = 1
 			var candidate [4]int
 
+			// Loop to search for candidates in the grid horizontals
 			for counter := 0; counter < 4; counter++ {
 				product *= number_list[i+counter]
 				candidate[counter] = number_list[i+counter]
 			}
 
+			// Update the greatest and great_list, if it's necessary
 			if greatest < product {
 				greatest = product
 				great_list = UpdateGreatestList(great_list, candidate)
@@ -92,14 +102,18 @@ func main() {
 		}
 
 		if VerticalVerify(i, len(number_list)) {
+
+			// Creation of variables to analyse the candidates to greatest product
 			var product int = 1
 			var candidate [4]int
 
+			// Loop to search for candidates in the grid verticals
 			for counter := 0; counter < 4; counter++ {
 				product *= number_list[i+20*counter]
 				candidate[counter] = number_list[i+20*counter]
 			}
 
+			// Update the greatest and great_list, if it's necessary
 			if greatest < product {
 				greatest = product
 				great_list = UpdateGreatestList(great_list, candidate)
@@ -108,14 +122,17 @@ func main() {
 
 		if DiagonalTypeOneVerify(i, len(number_list)) {
 
+			// Creation of variables to analyse the candidates to greatest product
 			var product int = 1
 			var candidate [4]int
 
+			// Loop to search for candidates on the diagonals (NW-SE) of the grid
 			for counter := 0; counter < 4; counter++ {
 				product *= number_list[i+20*counter-counter]
 				candidate[counter] = number_list[i+20*counter+counter]
 			}
 
+			// Update the greatest and great_list, if it's necessary
 			if greatest < product {
 				greatest = product
 				great_list = UpdateGreatestList(great_list, candidate)
@@ -124,14 +141,17 @@ func main() {
 
 		if DiagonalTypeTwoVerify(i) {
 
+			// Creation of variables to analyse the candidates to greatest product
 			var product int = 1
 			var candidate [4]int
 
+			// Loop to search for candidates on the diagonals (SW-NE) of the grid
 			for counter := 0; counter < 4; counter++ {
 				product *= number_list[i-20*counter-counter]
 				candidate[counter] = number_list[i-20*counter-counter]
 			}
 
+			// Update the greatest and great_list, if it's necessary
 			if greatest < product {
 				greatest = product
 				great_list = UpdateGreatestList(great_list, candidate)
@@ -139,6 +159,7 @@ func main() {
 		}
 	}
 
+	// Show results
 	fmt.Println(greatest)
 	fmt.Println(great_list)
 
