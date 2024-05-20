ff4596511d3f368e04c3663527c3a26e977f0cd8
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-20 14:10:05 +0000

    Trying to apply antoher logic

--- 00_Euler_Project/Exercise_12/12_Exercise.go
+++ 00_Euler_Project/Exercise_12/12_Exercise.go
@@ -1,33 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
+
+func NoContains(s []int, e int) bool {
+	for _, a := range s {
+		if a == e {
+			return false
+		}
+	}
+	return true
+}
 
 func DivisorCounter(n int) int {
-	var counter int = 1
-	for i := 1; i <= n/2; i++ {
+	divisor_list := make([]int, 0)
+	prime_list, fac_list := FactorizationList(n)
+	for i := 0; i < len(prime_list); i++ {
+		var divisor_counter int = 1
+		var j int = 0
+		for j < len(fac_list) {
+			if prime_list[i] == fac_list[j] {
+				fmt.Println(prime_list[i], fac_list[j])
+				divisor_list = append(divisor_list, divisor_counter)
+				divisor_counter++
+			} else {
+				j++
+			}
+		}
+
+	}
+
+	fmt.Println(prime_list, fac_list, divisor_list)
+	return 0
+}
+func FactorizationList(n int) ([]int, []int) {
+	fac_list := make([]int, 0)
+	prime_list := make([]int, 0)
+	var i int = 2
+	for n != 1 {
 		if n%i == 0 {
-			counter++
+			fac_list = append(fac_list, i)
+			if NoContains(prime_list, i) {
+				prime_list = append(prime_list, i)
+			}
+			n = n / i
+		} else {
+			i++
 		}
 	}
 
-	return counter
+	return fac_list, prime_list
 }
-
 func main() {
-	var triangle_number, great, i int = 0, 0, 1
-	for true {
-		triangle_number += i
-		if DivisorCounter(triangle_number) > great {
-			fmt.Println(triangle_number, DivisorCounter(triangle_number), i, great)
-			great = DivisorCounter(triangle_number)
-		}
-
-		if DivisorCounter(triangle_number) >= 500 {
-			break
-		}
-
-		i++
-	}
+	fmt.Println(DivisorCounter(48))
 }
 
-// Too slow
+// 76576500
