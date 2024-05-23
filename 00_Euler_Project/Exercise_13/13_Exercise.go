344ca6ebc8a19ea9d06d0833222e0d872329bff6
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-23 11:27:47 -0300

    Update 13_Exercise.go
    - logic changed
    - Use of Arrays
    - Bug fixing and optimization

--- 00_Euler_Project/Exercise_13/13_Exercise.go
+++ 00_Euler_Project/Exercise_13/13_Exercise.go
@@ -3,26 +3,57 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 )
 
+func FillSlice(a []int) []int {
+	for i := 0; i < 50; i++ {
+		a = append(a, 0)
+	}
+
+	return a
+}
+
 func main() {
+
 	// Read file
 	b, err := os.ReadFile("50digits.txt")
 	if err != nil {
 		fmt.Print(err)
 	}
 
-	// Convertion: bytes -> str
-	var j int = 0
-	c := make([]string, 50)
-	for i := 0; i < len(b)-1; i++ {
-		if b[i] == 10 {
-			j = 0
-			fmt.Println(c)
-		} else {
-			c[j] = string(b[i])
-			j++
+	// Create a var slice
+	var number_list []int
+
+	// Convertion: bytes -> str -> int
+	for i := 0; i < len(b); i++ {
+		if string(b[i]) != "\n" {
+			n, err := strconv.Atoi(string(b[i]))
+			if err != nil {
+				fmt.Print(err)
+			} else {
+				number_list = append(number_list, n)
+			}
 		}
 	}
 
+	var partial_sum, accumulated int = 0, 0
+	var a, c int = len(number_list) - 1, 49
+	var big_number []int
+
+	big_number = FillSlice([]int(big_number))
+
+	for i := c; i <= a; i += 50 {
+		partial_sum += number_list[i]
+		if partial_sum > 10 {
+			accumulated++
+			partial_sum -= 10
+		}
+		if i == a {
+			fmt.Println(partial_sum, accumulated)
+			fmt.Println(accumulated / 10)
+			break
+		}
+
+	}
 }
