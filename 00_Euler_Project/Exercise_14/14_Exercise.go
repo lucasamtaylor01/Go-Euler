495b35164cfab5a23868031f99879b4a5bb51ba2
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-25 15:09:48 -0300

    Searching phase

---
+++ 00_Euler_Project/Exercise_14/14_Exercise.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
+
+/*
+SEARCHING PHASE:
+
+	- http://arxiv.org/pdf/1510.01274
+	- https://www.jasondavies.com/collatz-graph/
+	- https://www.geogebra.org/m/s5eb2wcy
+	- https://oeis.org/A008908
+	- https://oeis.org/A064433
+	- https://en.wikipedia.org/wiki/Collatz_conjecture
+
+*/
