fbd715374621e97b7fb88f18b8d6df6c0eb6ef69
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-26 21:25:12 -0300

    Add coments

--- 00_Euler_Project/Exercise_14/14_Exercise.go
+++ 00_Euler_Project/Exercise_14/14_Exercise.go
@@ -1,13 +1,29 @@
+/*
+The following iterative sequence is defined for the set of positive integers:
+
+n -> n/2 (n is even)
+n -> 3n + 1 (n is odd)
+
+Using the rule above and starting with 13, we generate the following sequence:
+13 -> 40 -> 20 -> 10 -> 5 -> 16 -> 8 -> 4 -> 2 -> 1.
+
+It can be seen that this sequence (starting at 13 and finishing at 1) contains 10 terms. Although it has not been proved yet (Collatz Problem), it is thought that all starting numbers finish at 1.
+Which starting number, under one million, produces the longest chain?
+
+NOTE: Once the chain starts the terms are allowed to go above one million.
+*/
 package main
 
 import "fmt"
 
-// inocent algorithm
+// Verify the chain lenght
 func ChainLenght(n int) int {
 	var chain []int
 
 	for n != 1 {
 		chain = append(chain, n)
+
+		// Apply collatz number logic
 		if n%2 == 0 {
 			n = n / 2
 		} else {
@@ -19,7 +35,10 @@ func ChainLenght(n int) int {
 }
 
 func main() {
+	// Create variables to analyse the candidates
 	var biggest_chain, number_chain int = 0, 0
+
+	// Loop to analyse candidates under 10^6
 	for i := 2; i < 1000000; i++ {
 		var chain_len int = ChainLenght(i)
 		if biggest_chain < chain_len {
@@ -28,5 +47,8 @@ func main() {
 		}
 	}
 
+	// Show solution
 	fmt.Println(number_chain)
 }
+
+//Coments: I know that is not efficient. In future, rewrite the code can be a good ideia
