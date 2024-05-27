bbae8741920dd82c2116892236eb4d5fedcfa089
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-27 12:41:38 -0300

    Try to change the base
    And fail (again)

--- 00_Euler_Project/Exercise_16/16_Exercise.go
+++ 00_Euler_Project/Exercise_16/16_Exercise.go
@@ -7,23 +7,17 @@ import (
 	"math"
 )
 
-func power2(n int) float64 {
-	var base int = 1
-	for i := 0; i < n; i++ {
-		base *= 2
-	}
-	return float64(base)
-}
 func main() {
-	var n float64 = power2(100)
-	var k int = int(math.Log10(n))
-
-	var sum int = 0
+	var n float64 = math.Pow(2, 1000)
+	var k int = int(math.Log2(n))
+	var sum float64 = 0
 	for i := 0; i <= k; i++ {
-		d_numerator := ((int(n) % int(math.Pow(10, float64(i+1)))) - (int(n) % int(math.Pow(10, float64(i)))))
-		d_denominator := int(math.Pow(10, float64(i)))
-		d := d_numerator / d_denominator
-		sum += d
+		d_1 := int(n) % int(math.Pow(2, float64(i+1)))
+		d_2 := int(n) % int(math.Pow(2, float64(i)))
+		fmt.Println(d_1, d_2)
+		num := d_1 - d_2
+		den := int(math.Pow(2, float64(i)))
+		sum += math.Abs(float64(num / den))
 	}
 
 	fmt.Println(sum)
