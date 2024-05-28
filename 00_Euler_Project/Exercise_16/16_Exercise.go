61afe435c565413f7b268e5a74a200b8c6ff7332
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-28 11:42:55 -0300

    Discovering math/big

--- 00_Euler_Project/Exercise_16/16_Exercise.go
+++ 00_Euler_Project/Exercise_16/16_Exercise.go
@@ -1,25 +1,15 @@
-// https://en.wikipedia.org/wiki/Digit_sum
-
 package main
 
 import (
 	"fmt"
-	"math"
+	"math/big"
 )
 
 func main() {
-	var n float64 = math.Pow(2, 1000)
-	var k int = int(math.Log2(n))
-	var sum float64 = 0
-	for i := 0; i <= k; i++ {
-		d_1 := int(n) % int(math.Pow(2, float64(i+1)))
-		d_2 := int(n) % int(math.Pow(2, float64(i)))
-		fmt.Println(d_1, d_2)
-		num := d_1 - d_2
-		den := int(math.Pow(2, float64(i)))
-		sum += math.Abs(float64(num / den))
-	}
+	a := new(big.Int)
+	a.Exp(big.NewInt(2), big.NewInt(1000), nil)
 
-	fmt.Println(sum)
-
+	fmt.Println(a)
 }
+
+// https://pkg.go.dev/math/big
