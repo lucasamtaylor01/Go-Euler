a3b28fd425a19040f4fc7bee621fc2c9fd77e1ab
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-29 16:20:32 -0300

    Add coments

--- 00_Euler_Project/Exercise_16/16_Exercise.go
+++ 00_Euler_Project/Exercise_16/16_Exercise.go
@@ -7,11 +7,17 @@ import (
 )
 
 func main() {
+	//Define 2^100
 	a := new(big.Int)
 	a.Exp(big.NewInt(2), big.NewInt(1000), nil)
+
+	// Convert the big number to string
 	str := a.String()
 
+	// Define the sum
 	var sum int = 0
+
+	// Loop to sum the digits of 2^100
 	for i := 0; i < len(str); i++ {
 		n, err := strconv.Atoi(string(str[i]))
 		if err != nil {
@@ -21,5 +27,6 @@ func main() {
 		}
 	}
 
+	//Show solution
 	fmt.Println(sum)
 }
