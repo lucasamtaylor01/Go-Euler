4e33bc7928cfecb835b046c9056e153cc3a37722
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-29 15:40:34 -0300

    Update 16_Exercise.go

--- 00_Euler_Project/Exercise_16/16_Exercise.go
+++ 00_Euler_Project/Exercise_16/16_Exercise.go
@@ -3,13 +3,23 @@ package main
 import (
 	"fmt"
 	"math/big"
+	"strconv"
 )
 
 func main() {
 	a := new(big.Int)
 	a.Exp(big.NewInt(2), big.NewInt(1000), nil)
+	str := a.String()
 
-	fmt.Println(a)
+	var sum int = 0
+	for i := 0; i < len(str); i++ {
+		n, err := strconv.Atoi(string(str[i]))
+		if err != nil {
+			fmt.Print(err)
+		} else {
+			sum += n
+		}
+	}
+
+	fmt.Println(sum)
 }
-
-// https://pkg.go.dev/math/big
