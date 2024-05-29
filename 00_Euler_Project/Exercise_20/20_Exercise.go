1a3efb5660cda03eb1cbe0c47e1be387357431b0
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-05-29 16:23:50 -0300

    Add Exercise 20
    With solution and comments

---
+++ 00_Euler_Project/Exercise_20/20_Exercise.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"strconv"
+)
+
+// Source: https://stackoverflow.com/qustions/11270547/go-big-int-factorial-with-recursion
+func factorial(n *big.Int) (result *big.Int) {
+	b := big.NewInt(0)
+	c := big.NewInt(1)
+
+	if n.Cmp(b) == -1 {
+		result = big.NewInt(1)
+	}
+	if n.Cmp(b) == 0 {
+		result = big.NewInt(1)
+	} else {
+		result = new(big.Int)
+		result.Set(n)
+		result.Mul(result, factorial(n.Sub(n, c)))
+	}
+	return
+}
+
+func main() {
+	// Define 100!
+	a := factorial(big.NewInt(100))
+
+	// Convert the big Int to string
+	str := a.String()
+
+	// Define the sum
+	var sum int = 0
+
+	// Loop to sum the digits of 100!
+	for i := 0; i < len(str); i++ {
+		n, err := strconv.Atoi(string(str[i]))
+		if err != nil {
+			fmt.Print(err)
+		} else {
+			sum += n
+		}
+	}
+
+	// Show solution
+	fmt.Println(sum)
+}
