dfbca2daa8b0a53dd1a099c7ff087e686e3d592e
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-06-04 23:48:07 -0300

    Update 18_Exercise.go

--- 00_Euler_Project/Exercise_18/18_Exercise.go
+++ 00_Euler_Project/Exercise_18/18_Exercise.go
@@ -1,44 +1,41 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
-	"strings"
+    "bufio"
+    "fmt"
+    "os"
+    "strconv"
+    "strings"
 )
 
 func main() {
-	// Abre o arquivo
-	file, err := os.Open("triangle.txt")
-	if err != nil {
-		fmt.Println("Erro ao abrir o arquivo:", err)
-		return
-	}
-	defer file.Close()
+    // Open the file
+    file, err := os.Open("triangle.txt")
+    if err != nil {
+        fmt.Println("Error opening file:", err)
+        return
+    }
+    defer file.Close()
 
-	var matrix [][]int
-	scanner := bufio.NewScanner(file)
+    var matrix [][]int
+    scanner := bufio.NewScanner(file)
 
-	// Lê o arquivo linha por linha
-	for scanner.Scan() {
-		line := scanner.Text()
-		strNumbers := strings.Fields(line)
-		var row []int
+    // Read the file line by line
+    for scanner.Scan() {
+        line := scanner.Text()
+        strNumbers := strings.Fields(line)
+        var row []int
 
-		// Converte cada string para int
-		for _, str := range strNumbers {
-			num, _ := strconv.Atoi(str)
-			row = append(row, num)
-		}
-		matrix = append(matrix, row)
-	}
+        // Convert each string to int
+        for _, str := range strNumbers {
+            num, _ := strconv.Atoi(str)
+            row = append(row, num)
+        }
+        matrix = append(matrix, row)
+    }
 
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Erro ao ler o arquivo:", err)
-		return
-	}
-
-	fmt.Println(matrix[1][0])
-
+    if err := scanner.Err(); err != nil {
+        fmt.Println("Error reading file:", err)
+        return
+    }
 }
