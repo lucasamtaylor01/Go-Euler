c70f217ad289f525115dc4ab80383aa9e83e5858
Lucas Amaral Taylor <87903796+lucasamtaylor01@users.noreply.github.com>
2024-06-08 22:26:18 -0300

    big step forward

--- 00_Euler_Project/Exercise_18/18_Exercise.go
+++ 00_Euler_Project/Exercise_18/18_Exercise.go
@@ -40,32 +40,23 @@ func main() {
 	}
 
 	var l int = 1
-	var ceil_i, ceil_j, ceil_k int = len(m[l]), len(m[l]), len(m[l+1])
 	var floor_i, floor_j, floor_k int = 0, 0, 0
-	for i := floor_i; i < ceil_i; i++ {
-		for j := floor_j; j < ceil_j; j++ {
-			for k := floor_k; k < floor_k+2; k++ {
+	for i := floor_i; i < floor_i+2; i++ {
+		for j := floor_j; j < floor_j+2; j++ {
+			if floor_k+2 >= len(m[l+2]) {
+				floor_k--
+			}
+			for k := floor_k; k <= floor_k+2; k++ {
 				fmt.Println(m[l][i], m[l+1][j], m[l+2][k])
 				fmt.Println(l, ":", i, "/", l+1, ":", j, "/", l+2, ":", k)
-				fmt.Println(floor_k, ":", ceil_k)
 			}
 			floor_k++
+
 		}
 		floor_j++
-		ceil_j++
+		fmt.Println()
 	}
 	floor_i++
-	ceil_i++
 	l++
 
 }
-
-/*
-	95-17-18
-	95-17-35
-	95-47-18
-	95-47-35
-	**64-17-18
-	64-17-35
-	64-47-
-*/
